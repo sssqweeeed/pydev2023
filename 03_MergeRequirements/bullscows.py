c4e1ed478fb227955555593b677dcbfc46e4f0f3
Mikhail Voronin <mischav44@gmail.com>
2023-02-26 17:17:38 +0300

    Add "Bulls and cows"

---
+++ 03_MergeRequirements/bullscows.py
@@ -0,0 +1,89 @@
+import os.path
+import random
+from argparse import ArgumentParser
+import urllib.request
+import re
+
+
+def bullscows(guess: str, secret: str) -> (int, int):
+    assert len(guess) == len(secret), 'Wrong len'
+
+    bulls = sum(map(lambda a, b: a == b, guess, secret))
+    cows = len(set(guess).intersection(set(secret)))
+
+    return bulls, cows
+
+
+def ask(prompt: str, valid: list[str] = None) -> str:
+    while True:
+        guess = input(prompt)
+        if valid is not None:
+            if guess not in valid:
+                continue
+        break
+    return guess
+
+
+def inform(format_string: str, bulls: int, cows: int) -> None:
+    print(format_string.format(bulls, cows))
+
+
+def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
+    secret = random.choice(words)
+    print(secret)
+    counter = 0
+
+    while True:
+        guess = ask("Введите слово: ", words)
+        counter += 1
+        bulls, cows = bullscows(guess, secret)
+        inform("Быки: {}, Коровы: {}", bulls, cows)
+        if guess == secret:
+            # print(f'Количество попыток: {counter}')
+            break
+    return counter
+
+
+parser = ArgumentParser(
+    prog='Bulls and cows'
+)
+
+parser.add_argument(
+    '-d', dest='d', action='store',
+    default='https://raw.githubusercontent.com/Harrix/Russian-Nouns/main/dist/russian_nouns.txt',
+    type=str,
+    help=""
+)
+
+parser.add_argument(
+    '-l', dest='l', action='store', default=5, type=int,
+    help="", required=False
+)
+
+# https://stackoverflow.com/questions/7160737/how-to-validate-a-url-in-python-malformed-or-not
+regex = re.compile(
+    r'^(?:http|ftp)s?://'  # http:// or https://
+    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+    r'localhost|'  # localhost...
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+    r'(?::\d+)?'  # optional port
+    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    words = None
+
+    if re.match(regex, args.__dict__['d']) is not None:
+        # url
+        with urllib.request.urlopen(args.__dict__['d']) as f:
+            words = f.read().decode('utf-8').splitlines()
+    else:
+        # file
+        if os.path.exists(args.__dict__['d']):
+            with open(args.__dict__['d'], 'r', encoding="utf-8") as f:
+                words = f.read().splitlines()
+        else:
+            print('File not exists')
+    words = list(filter(lambda word: len(word) == args.__dict__['l'], words))
+    count = gameplay(ask, inform, words=words)
+    print(f'Количество попыток: {count}')
